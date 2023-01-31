37d84ae1b5cf269006f588edb046cf2ef56e759d
prakash-a7x <prakash.a7x@gmail.com>
2023-01-31 11:04:27 +0600

    functionality changed

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ $redis = new Redis();
 //Connecting to Redis
 $redis->connect('127.0.0.1', 6379);
 
-if ($redis->ping()) {
- echo "PONG";
+if ($redis->ping())
+{
+	echo "PONG\n";
 }
+
+$key = "Key_Name";
+$redis->set($key, 'Key Value');
+echo $redis->get($key);
--- subscribe.php
+++ subscribe.php
@@ -3,11 +3,14 @@
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 
-$redis->subscribe(['chan-1', 'chan-2'], function ($redis, $chan, $msg) {
+$redis->subscribe(['chan-1', 'chan-2'], 'subscribe');
+
+function subscribe($redis, $chan, $msg)
+{
 	echo "$msg on $chan", PHP_EOL;
 	// write on a file 
 	$myFile = fopen("redis_sub.txt", "a") or die("Unable to open file!");
 	$txt = "$msg on $chan\n";
 	fwrite($myFile, $txt);
 	fclose($myFile);
-});
+};
